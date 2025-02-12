7cde8e2a5338e3bb48e56ec6a0f2aea418d1b8cf
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-12 12:22:37 +0100

    feat: install axios - add Index movies to API

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
+import { useSearchBarContext } from "../contexts/SearchBarContext";
+import axios from "axios";
 
 export default function SearchBar() {
   const [search, setSearch] = useState("");
 
+  const { setMovies } = useSearchBarContext();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: "a2a818323e4aed568180bfa536321829",
+          query: search,
+        },
+      })
+      .then((res) => setMovies(res.data.results));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSearch}>
       <input
         name="search"
         type="search"
