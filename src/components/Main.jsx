2120467bc5fc31ac556598ca8ab63cc213176773
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-12 12:39:12 +0100

    feat: add movies list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,35 @@
+import { useSearchBarContext } from "../contexts/SearchBarContext";
+
 export default function Main() {
+  const { movies } = useSearchBarContext();
+
   return (
     <main>
-      <h2>Lista film</h2>
+      <h2>Lista Film</h2>
+      <ul>
+        {movies.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <h3>
+                <strong>Titolo: </strong>
+                {movie.title}
+              </h3>
+              <h4>
+                <strong>Titolo originale: </strong>
+                {movie.original_title}
+              </h4>
+              <h4>
+                <strong>Lingua originale: </strong>
+                {movie.original_language}
+              </h4>
+              <h4>
+                <strong>Media voto: </strong>
+                {movie.vote_average}
+              </h4>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
