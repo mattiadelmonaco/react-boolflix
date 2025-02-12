a704f965a49049037526142cca02d9d528708505
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-12 20:51:08 +0100

    feat: add alert when click on nav list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
 import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 export default function Header() {
+  const [alert, setAlert] = useState({ message: "" });
+
+  const showAlert = () => {
+    setAlert({ message: "E' un tasto finto" });
+
+    setTimeout(() => {
+      setAlert({ message: "" });
+    }, 3000);
+  };
+
+  const closeAlert = () => {
+    setAlert({ message: "" });
+  };
+
   return (
     <header>
       <div className="container">
@@ -20,16 +35,16 @@ export default function Header() {
             />
           </a>
           <ul className="flex gap-8">
-            <li className="hidden lg:block">
+            <li className="hidden lg:block" onClick={showAlert}>
               <a href="#">Home</a>
             </li>
-            <li className="hidden lg:block">
+            <li className="hidden lg:block" onClick={showAlert}>
               <a href="#">Serie TV</a>
             </li>
-            <li className="hidden lg:block">
+            <li className="hidden lg:block" onClick={showAlert}>
               <a href="#">Film</a>
             </li>
-            <li className="hidden lg:block">
+            <li className="hidden lg:block" onClick={showAlert}>
               <a href="#">La mia lista</a>
             </li>
           </ul>
@@ -37,6 +52,21 @@ export default function Header() {
           <SearchBar />
         </div>
       </div>
+
+      {/* ALERT */}
+
+      {alert.message && (
+        <div className="text-white min-w-70 p-3 shadow-2xl fixed bottom-15 right-15 rounded-lg bg-red-900">
+          <div className="flex justify-between items-center">
+            <i className="fa-solid fa-triangle-exclamation"></i>
+            <p>{alert.message}</p>
+            <i
+              className="fa-solid fa-xmark cursor-pointer px-2"
+              onClick={closeAlert}
+            ></i>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
